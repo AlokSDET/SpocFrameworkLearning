1c6234d6bd13fcdff017e6f82feffc2781528f83
chitransh0501 <45283964+chitransh0501@users.noreply.github.com>
2020-02-03 15:17:19 +0530

    formating file

--- src/test/java/groovyTests/GroovyAdd.groovy
+++ src/test/java/groovyTests/GroovyAdd.groovy
@@ -3,14 +3,14 @@ package groovyTests;
 
 import java.util.Random;
 public class SumOfTwoNumber{
-	
+
 	public SumOfTwoNumber() {
 		int i = new Random().nextInt();
 		println(i);
 	}
-	
-	
-	
+
+
+
 	public int add(int a, int b) {
 		return a + b;
 	}
